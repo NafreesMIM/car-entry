e6659f25b347a49a5b5f7f6c5daf71b7d66672fc
NafreesMIM <143109431+NafreesMIM@users.noreply.github.com>
2024-06-22 12:00:23 +0530

    FU

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Car_Entry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCarName = new System.Windows.Forms.TextBox();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
+            this.numericUpDownYear = new System.Windows.Forms.NumericUpDown();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dgvt = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.77309F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.1029F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.124011F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 163F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxCarName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxModel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownYear, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonInsert, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonUpdate, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDelete, 3, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(592, 142);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Car Name";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Model No";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year of Manufacture";
+            // 
+            // textBoxCarName
+            // 
+            this.textBoxCarName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCarName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCarName.Location = new System.Drawing.Point(147, 12);
+            this.textBoxCarName.Name = "textBoxCarName";
+            this.textBoxCarName.Size = new System.Drawing.Size(247, 23);
+            this.textBoxCarName.TabIndex = 3;
+            // 
+            // textBoxModel
+            // 
+            this.textBoxModel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxModel.Location = new System.Drawing.Point(147, 59);
+            this.textBoxModel.Name = "textBoxModel";
+            this.textBoxModel.Size = new System.Drawing.Size(247, 23);
+            this.textBoxModel.TabIndex = 4;
+            // 
+            // numericUpDownYear
+            // 
+            this.numericUpDownYear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownYear.Location = new System.Drawing.Point(147, 106);
+            this.numericUpDownYear.Name = "numericUpDownYear";
+            this.numericUpDownYear.Size = new System.Drawing.Size(247, 23);
+            this.numericUpDownYear.TabIndex = 5;
+            this.numericUpDownYear.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInsert.Location = new System.Drawing.Point(430, 3);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(159, 41);
+            this.buttonInsert.TabIndex = 6;
+            this.buttonInsert.Text = "Save";
+            this.buttonInsert.UseVisualStyleBackColor = false;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(430, 50);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(159, 41);
+            this.buttonUpdate.TabIndex = 7;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(430, 97);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(159, 42);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dgvt
+            // 
+            this.dgvt.AllowUserToAddRows = false;
+            this.dgvt.AllowUserToDeleteRows = false;
+            this.dgvt.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvt.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvt.Location = new System.Drawing.Point(0, 149);
+            this.dgvt.Name = "dgvt";
+            this.dgvt.ReadOnly = true;
+            this.dgvt.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvt.Size = new System.Drawing.Size(592, 256);
+            this.dgvt.TabIndex = 1;
+            this.dgvt.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvt_RowHeaderMouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 405);
+            this.Controls.Add(this.dgvt);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Car Entry";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCarName;
+        private System.Windows.Forms.TextBox textBoxModel;
+        private System.Windows.Forms.NumericUpDown numericUpDownYear;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridView dgvt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Entry
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnct=new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=CarModels;Integrated Security=True");
+        DataTable dt;
+        SqlDataAdapter adapter;
+        SqlCommand cmd;
+        int carid;
+        public Form1()
+        {
+            InitializeComponent();
+            myCode();
+            dgvt.Columns[0].Visible = false;
+        
+        }
+        void myCode()
+        {
+            cnct.Open();
+            dt = new DataTable();
+            adapter = new SqlDataAdapter("select id,Carname,CarModel,Year from carDetails", cnct);
+            adapter.Fill(dt);
+            dgvt.DataSource = dt;
+            cnct.Close();
+        }
+        void Clear()
+        {
+            textBoxCarName.Clear();
+            textBoxModel.Clear();
+            numericUpDownYear.Value = 1;
+            textBoxCarName.Select();
+        }
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            
+            if (textBoxCarName.Text!=string.Empty && textBoxModel.Text!=string.Empty && numericUpDownYear.Value!=0)
+            {
+                cnct.Open();
+                cmd = new SqlCommand("INSERT INTO carDetails(CarName,CarModel,Year) Values(@name,@model,@year)", cnct);
+                cmd.Parameters.AddWithValue("@name", textBoxCarName.Text);
+                cmd.Parameters.AddWithValue("@model", textBoxModel.Text);
+                cmd.Parameters.AddWithValue("@year", numericUpDownYear.Value);
+                cmd.ExecuteNonQuery();
+                cnct.Close();
+                MessageBox.Show("insert Successfully!");
+                myCode();
+                
+            }
+            else
+            {
+                MessageBox.Show("Please Enter All Details!");
+            }
+            
+            
+            
+
+        }
+
+        private void dgvt_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            cnct.Open();
+            carid =int.Parse(dgvt.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBoxCarName.Text = dgvt.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBoxModel.Text= dgvt.Rows[e.RowIndex].Cells[2].Value.ToString();
+            numericUpDownYear.Value=int.Parse(dgvt.Rows[e.RowIndex].Cells[3].Value.ToString());
+            cnct.Close();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            
+            if (textBoxCarName.Text != string.Empty && textBoxModel.Text != string.Empty && numericUpDownYear.Value != 0)
+            {
+                cnct.Open();
+                cmd = new SqlCommand("UPDATE carDetails SET CarName=@name,CarModel=@model,Year=@year WHERE id=@Id", cnct);
+                cmd.Parameters.AddWithValue("@Id", carid);
+                cmd.Parameters.AddWithValue("@name", textBoxCarName.Text);
+                cmd.Parameters.AddWithValue("@model", textBoxModel.Text);
+                cmd.Parameters.AddWithValue("@year", numericUpDownYear.Value);
+                cmd.ExecuteNonQuery();
+                cnct.Close();
+                MessageBox.Show("Updated Successfully!");
+                myCode();
+                Clear();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Enter All Details!");
+            }
+
+            
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            
+            if (textBoxCarName.Text != string.Empty && textBoxModel.Text != string.Empty)
+            {
+                cnct.Open();
+                cmd = new SqlCommand("DELETE carDetails  WHERE Id=@Id", cnct);
+                cmd.Parameters.AddWithValue("@Id", carid);
+                cmd.Parameters.AddWithValue("@name", textBoxCarName.Text);
+                cmd.Parameters.AddWithValue("@model", textBoxModel.Text);
+                cmd.Parameters.AddWithValue("@year", numericUpDownYear.Value);
+                cmd.ExecuteNonQuery();
+                cnct.Close();
+                MessageBox.Show("Data Deleted Successfully!");
+                myCode();
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please Select any data!");
+                textBoxCarName.Select();
+            }
+            
+        }
+    }
+}
